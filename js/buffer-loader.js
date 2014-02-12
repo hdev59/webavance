01896330af7b42ba3807324d115dfde193e9a4a7
Hervé DEVOS <herve.devos@atos.net>
2014-02-12 16:40:11 +0100

    sparql

--- js/buffer-loader.js
+++ js/buffer-loader.js
@@ -32,9 +32,7 @@ BufferLoader.prototype.loadBuffer = function(url, index) {
 			}});
 		
 		var dv = new jDataView(request.response);
-		console.log("======== jDataView =========");
-		console.log(dv);
-			
+
 		if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
 			console.log('tag found');
 			var title = dv.getString(30, dv.tell());
@@ -48,9 +46,20 @@ BufferLoader.prototype.loadBuffer = function(url, index) {
 			if (artist != "") {
 				querySparqlArtist(artist);
 			}
-			$('#track-info-content-' + index)[0].innerHTML = '<p>Album : ' + album + '</p><p> Artist : ' + artist + '</p><p>Year : ' + year + "</p><p>Title : " + title + "</p>";
+			var trackInfoContent = '<p>Album : ' + album + '</p><p> Artist : ' + artist + '</p><p>Year : ' + year + "</p><p>Title : " + title + '</p>';
+			if (sparqlResults['page']) {
+				trackInfoContent += '<p><a href="' + sparqlResults['page'] + '">Page wikipedia</a></p>';
+			}
+			if (sparqlResults['thumbnail']) {
+				trackInfoContent += '<p>Image artiste : </p> <img src="' + sparqlResults['thumbnail'] + '" />';
+			}
+			if (sparqlResults['abstract']) {
+				trackInfoContent += '<p>Description : ' + sparqlResults['abstract'] + '</p>';
+			}
+			$('#track-info-content-' + index)[0].innerHTML =  trackInfoContent;
 			
-			trackTags[index] = { 'album' : album, 'artist' : artist, 'title' : title, 'year' : year };
+			trackTags[index] = { 'album' : album, 'artist' : artist, 'title' : title, 'year' : year, 'thumbnail' : sparqlResults['thumbnail'], 'page' : sparqlResults['page'], 'abstract' : sparqlResults['abstract'] };
+		
 		} else {
 			console.log('tag not found');
 		}
@@ -123,33 +132,48 @@ BufferLoader.prototype.loadBuffer = function(url, index) {
     request.send();
 }
 
-var sparqlResults;
+var sparqlResults = [];
 
 function querySparqlArtist(artist) {
+	sparqlResults = [];
 	var query = encodeURIComponent(artist).replace(/%20/g,'+').replace(/%00/g,'');
-	var  queryUrl = "http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=select+%3Fy+%3Fz+where%0D%0A+%7B+%3Fr+%3Fy+%3Fz.%0D%0A+++%3Fr+foaf%3Aname+%22"+query+"%22%40en%7D&format=application%2Fsparql-results%2Bjson&timeout=&debug=on"
-
+	var queryResource = encodeURIComponent(artist).replace(/%20/g,'_').replace(/%00/g,'');
+	var queryUrl = "http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=select+%3Fy+%3Fz+where%0D%0A+%7B+%3Fr+%3Fy+%3Fz.%0D%0A+++%3Fr+foaf%3Aname+%22"+query+"%22%40en%7D&format=application%2Fsparql-results%2Bjson&timeout=&debug=on"
+	var queryBisUrl = "http://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=select+%3Fabstract+%3Fthumbnail+%3Fpage+where%0D%0A+%7Bdbpedia%3A"+queryResource+"+dbpedia-owl%3Aabstract+%3Fabstract%3B%0D%0Afoaf%3AisPrimaryTopicOf+%3Fpage%3B%0D%0Adbpedia-owl%3Athumbnail+%3Fthumbnail.%0D%0AFILTER%28langMatches%28lang%28%3Fabstract%29%2C%22fr%22%29%29%7D+&format=application%2Fsparql-results%2Bjson&timeout=30000&debug=on"
+	
 	$.ajax({
 	  dataType: "json",
-	  url: queryUrl,
+	  url: queryBisUrl,
 	  async: false,
 	  error : errorQuery,
 	  success: successQuery
 	});
 	
 	function successQuery(data) {
-		sparqlResults = data;
-	}
-	
-	function errorQuery(e) {
-		if (e.statusText == 'OK') {
-			sparqlResults = e.responseText;
+		if (data.results && data.results.bindings[0] && data.results.bindings[0].abstract && data.results.bindings[0].abstract.value) {
+			sparqlResults['abstract'] = data.results.bindings[0].abstract.value;
+		} else {
+			sparqlResults['abstract'] = '';
+		}
+		if (data.results && data.results.bindings[0] && data.results.bindings[0].thumbnail && data.results.bindings[0].thumbnail.value) {
+			sparqlResults['thumbnail'] = data.results.bindings[0].thumbnail.value;
+		} else {
+			sparqlResults['thumbnail'] = '';
+		}
+		if (data.results && data.results.bindings[0] && data.results.bindings[0].page && data.results.bindings[0].page.value) {
+			sparqlResults['page'] = data.results.bindings[0].page.value;
+		} else {
+			sparqlResults['page'] = '';
 		}
 	}
 	
-	//console.log("SPARQL QUERY RESULTS");
-	//console.log(sparqlResults);
-
+	function errorQuery(e) {
+		console.log('Could not retrieve dbpedia data');
+		sparqlResults['abstract'] = '';
+		sparqlResults['thumbnail'] = '';
+		sparqlResults['page'] = '';
+		
+	}
 }
 
 
